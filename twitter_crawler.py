d1d394dd2c47660333e6bff66af716259f7784d0
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2020-02-28 09:54:54 +0800

    add header in data parsing

--- twitter_crawler.py
+++ twitter_crawler.py
@@ -106,7 +106,21 @@ def parse_media(media):
         source_status_id = media_.get('source_status_id')
         type = media_.get('type')
         url = media_.get('url')
-        data = requests.get(media_url).content # need to check it
+
+        headers = {
+            'authority': 'pbs.twimg.com',
+            'cache-control': 'max-age=0',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
+            'sec-fetch-dest': 'document',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'none',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
+            'if-modified-since': 'Fri, 28 Feb 2020 00:21:44 GMT',
+        }
+        data = requests.get(media_url,headers=headers).content # need to check it
 
         result.append([display_url, expanded_url, id, indices, media_url, media_url_https, source_status_id, type, url, data])
 
