1916176d1c92852c50e0343a713eb0eb2fca05e9
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2020-02-28 15:11:58 +0800

    add exception and retry

--- twitter_crawler.py
+++ twitter_crawler.py
@@ -107,20 +107,29 @@ def parse_media(media):
         type = media_.get('type')
         url = media_.get('url')
 
-        headers = {
-            'authority': 'pbs.twimg.com',
-            'cache-control': 'max-age=0',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
-            'sec-fetch-dest': 'document',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'sec-fetch-site': 'none',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-user': '?1',
-            'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
-            'if-modified-since': 'Fri, 28 Feb 2020 00:21:44 GMT',
-        }
-        data = requests.get(media_url,headers=headers).content # need to check it
+        data_retry_count=0
+        while data_retry_count<5:
+            try:
+                headers = {
+                    'authority': 'pbs.twimg.com',
+                    'cache-control': 'max-age=0',
+                    'upgrade-insecure-requests': '1',
+                    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36',
+                    'sec-fetch-dest': 'document',
+                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                    'sec-fetch-site': 'none',
+                    'sec-fetch-mode': 'navigate',
+                    'sec-fetch-user': '?1',
+                    'accept-language': 'en-US,en;q=0.9,ko;q=0.8',
+                    'if-modified-since': 'Fri, 28 Feb 2020 00:21:44 GMT',
+                }
+                data = requests.get(media_url,headers=headers).content
+                break
+            except:
+                data_retry_count+=1
+                sleep(60)
+                logging.warning('Failed to get media data. wait for 60 secs... [{0}]'.format(data_retry_count+1))
+
 
         result.append([display_url, expanded_url, id, indices, media_url, media_url_https, source_status_id, type, url, data])
 
